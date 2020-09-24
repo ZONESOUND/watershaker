7c24e7902d623004e1784334eb5d300ab24fed93
lowlow <joyu.lo.tw@gmail.com>
2020-09-24 17:09:27 +0800

    remove console in pageHeight

--- src/pageHeight.js
+++ src/pageHeight.js
@@ -3,13 +3,12 @@ window.addEventListener('resize', changeSize);
 function changeSize() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-    console.log('vh', vh);
+    //console.log('vh', vh);
     putSelector();
 }
 changeSize();
 
 export function putSelector(){
-    console.log(window.innerHeight, window.innerWidth);
     let vh = window.innerHeight > window.innerWidth ? window.innerHeight : window.innerWidth;
     let f = document.getElementById('footer').clientHeight;
     if (f == 0) return;
@@ -18,8 +17,6 @@ export function putSelector(){
     let s = document.getElementById('selector').clientHeight;
     let margin = (vh - (t+st+s+f))/3 * 0.01;
     if (margin < 0) margin = 0;
-    console.log(vh, f, t, st, s);
     document.documentElement.style.setProperty('--sub', `${margin}px`);
-    console.log(margin);
 }
 
